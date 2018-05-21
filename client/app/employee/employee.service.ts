c017ded2a26b9e6354d5155c8aeb4dfee93902bf
park <wjdghks158@naver.com>
2018-05-21 12:31:18 +0900

    init

--- client/app/employee/employee.service.ts
+++ client/app/employee/employee.service.ts
@@ -3,7 +3,7 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { BaseService } from '../shared/services';
+import { BaseService } from '../shared/services/base.service';
 
 @Injectable()
 export class EmployeeService {
