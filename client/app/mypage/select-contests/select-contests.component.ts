dd1972775745c002727c8debd5fb0379cb901ac1
park <wjdghks158@naver.com>
2018-05-21 13:39:01 +0900

    init

--- client/app/mypage/select-contests/select-contests.component.ts
+++ client/app/mypage/select-contests/select-contests.component.ts
@@ -1,158 +1,88 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ToastComponent } from '../../shared/toast/toast.component';
-import { ContestService } from '../../shared/services/contest.service';
-import { MatchService, AuthService } from '../../shared/services';
-import { UserService } from '../../shared/services/user.service';
+import { AuthService, UserService, ContestService } from '../../shared/services';
+
+import { OrderPipe } from 'ngx-order-pipe';
+
+
 
 @Component({
-  selector: 'app-show-contest',
-  templateUrl: './show-contest.component.html',
-  styleUrls: ['./show-contest.component.scss']
+  selector: 'app-select-contests',
+  templateUrl: './select-contests.component.html',
+  styleUrls: ['./select-contests.component.css']
 })
-export class ShowContestComponent implements OnInit {
+export class SelectContestsComponent implements OnInit {
 
-  userpages = { _id: '', contestid: '' };
-  currentDate = new Date();
-  interval;
-
-  //추천 공모전
+  cleanpages = [];
   contests = [];
+  isLoading = false;
 
-  
-  //선택된 카테고리
-  selectedCategory=[];
-  value = '';
 
-  //contest ID값
-  id: string;
-
-  //현재 공모전
-  contest = {
-    title: '',		// 글 제목
-    host: '',		// 주최기관
-    progress: '',		// 진행상황
-    hits: '',		// 조회수
-    contents: '',						// 글 내용
-    writer: '',							// 글쓴 사람
-    comments: [{		// 댓글
-        contents:'',					// 댓글 내용
-        writer: '',
-        created_at: ''
-    }],
-    startline: '', //공모전 시작일
-    deadline: '',  //공모전 마감일
-    person: '',  // 참여 인원
-    field : '', //분야
-    tags: '', // tag 이름
-    url : '', //사이트 url
-    url_img: '', // 그림 url
-    url_img2: '', // 그림 url
-    created_at: '', // 글 생성 시간
-    updated_at: ''  // 글 수정 시간
-  };
-  isLoading = true;
-
-  //매칭 글 
-  matchs = [];
   //Pipe
-  order: string = 'created_at';
-  reverse: boolean = true;
+order: string = 'created_at';
+reverse: boolean = true;
 
-  //page 넘버링 할 때 쓰는 변수
-  p:number = 1;
-  total: number;
-  
-  constructor(private _route: ActivatedRoute, public toast: ToastComponent,
-    private contestService: ContestService,private matchService: MatchService,
-    public auth: AuthService,
-    private userService: UserService) { }
+nonSelect: boolean = true;
+p:number = 1;
+total: number;
+
+
+  constructor(private auth: AuthService,
+  private contestService: ContestService,
+  private orderPipe: OrderPipe) {
+    console.log(this.auth.currentUser.pages);
+   }
 
   ngOnInit() {
-    this.id = this._route.snapshot.params['id'];
-    this.getContest(this.id);
-    this.getMatchs(this.id);
-    this.categorySearch();
-    this.userpages._id = this.auth.currentUser._id;
-    this.userpages.contestid = this.id;
-    this.addUserPages(this.userpages); //줘야할 변수는 contestID 값이랑 auth id 값
+    this.cleanPage();
+    this.getContest();
   }
 
-  getContest(id) {
-    this.contestService.getContest(id).subscribe(
-      data => this.contest = data,
-      error => console.log(error),
-      () => {
-        this.isLoading = false;
-        console.log(this.contest.tags);
-        this.selectedCategory = (this.contest.tags).split(",",1);
-        console.log("실험중");
-        if(this.contest.url_img2 == null) {
-          console.log("비었군요");
+
+  cleanPage() {
+    console.log('첫시작');
+    console.log(this.cleanpages.length);
+    for (var i = 0; i<this.auth.currentUser.pages.length; i++) {
+      var isSearch = false;
+      if(!(this.cleanpages.length == 0) ) {
+        console.log("이젠 0이 아니야 ");
+        console.log(this.cleanpages.length);
+        for( var k =0; k< this.cleanpages.length; k ++) {
+          if(this.cleanpages[k].contestid == this.auth.currentUser.pages[i]) {
+            isSearch = true;
+          }
         }
-        if(this.contest.url_img2 != null) {
-          console.log(this.contest.url_img2);
-        }
-        console.log(this.contest.url_img2);
-        console.log(this.selectedCategory[0]);
       }
-    );
-  }
-  //match에서 contest_id값을 이용하여 검색
-  getMatchs(id) {
-
-    var url ='/api/searchmatchs';
-    var a = "contest_id"
-    var b = id;
-
-
-    this.matchService.search(url+"?"+a+"="+b).subscribe(
-      data => this.matchs = data,
-      error => console.log(error),
-      () => {
-        this.isLoading = false;
-        console.log(Object.keys(this.matchs).length);
-        
+      console.log('여기 오잖아.');
+      if(!isSearch) {
+        this.cleanpages.push({contestid: this.auth.currentUser.pages[i]});
+        console.log('푸쉬완료.');
+        console.log(this.cleanpages.length);
       }
-    );
+      
+    }
   }
-  categorySearch(){
-    this.contests = [];
-    this.selectedCategory;
-    
-      var url ='/api/searchcontests';
 
-      var a = "tags";
-  
-      this.contestService.search(url+"?"+a+"="+this.selectedCategory).subscribe(
-        data => this.contests = data,
+
+  getContest() {
+    for (var i=0; i<this.cleanpages.length; i++) {
+      //console.log(this.recommendContests[i].contestid);
+      this.contestService.getContest(this.cleanpages[i].contestid).subscribe(
+        data => this.contests.push(data),
         error => console.log(error),
         () => {
+          //console.log(this.contests);
           this.isLoading = false;
-          console.log(Object.keys(this.contests).length);
-          //this.router.navigate(["/search/keyword?" + this.value]);
+          this.nonSelect = false;
         }
       );
+    }
   }
 
   setOrder(value: string){
     if(this.order === value){
       this.reverse = !this.reverse;
     }
+    
     this.order = value;
-  }
-
-  addUserPages(userpage) {
-    this.userService.addUserPages(userpage).subscribe(
-      res => this.toast.setMessage('account settings saved!', 'success'),
-      error => console.log(error),
-      () => {
-        this.auth.currentUser.pages.push(userpage.contestid);
-      }
-
-      
-    );
-  }
-
-
+    }
 }
